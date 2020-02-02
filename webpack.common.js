a4c7744632806186b3c4f87a19dda6d261eca920
jomar.cardoso <jomar.cardoso@cwi.com.br>
2020-02-02 18:42:59 -0300

    remove not used files

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,10 @@
-const path = require('path');
-
 module.exports = {
   entry: {
     index: './src/index.js',
   },
   output: {
     // path: 'dist',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
