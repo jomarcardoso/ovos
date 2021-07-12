01b08c5f78eaaca4f9e32ba78e5a4e5e52d4e10d
jomarcardoso <antonionetocardoso@gmail.com>
2021-07-11 23:55:14 -0300

    feat: 🎸 remove old position files

--- src/v2/utilities/axis/axis.spec.ts
+++ src/v2/utilities/axis/axis.spec.ts
@@ -5,6 +5,10 @@ import {
   isAboveTheScreen,
   isBelowTheScreen,
   isOnTheSameDirection,
+  isOnTheRegion,
+  getRelativePosition,
+  isSafe,
+  isOnGapOfEl,
 } from './axis';
 
 describe('DirectionService', () => {
@@ -370,6 +374,520 @@ describe('DirectionService', () => {
 
     it('position 1000 is below the screen 768px', () => {
       expect(isBelowTheScreen(1000)).toBe(true);
+    });
+  });
+
+  describe('isOnTheRegion', () => {
+    it('above the region: false', () => {
+      const result = isOnTheRegion({
+        position: { x: 0, y: 5 },
+        region: {
+          bottom: 15,
+          left: 0,
+          right: 0,
+          top: 10,
+        },
+      });
+
+      expect(result).toBe(false);
+    });
+
+    it('below the region: false', () => {
+      const result = isOnTheRegion({
+        position: { x: 0, y: 20 },
+        region: {
+          bottom: 15,
+          left: 0,
+          right: 0,
+          top: 10,
+        },
+      });
+
+      expect(result).toBe(false);
+    });
+
+    it('on top of the region: true', () => {
+      const result = isOnTheRegion({
+        position: { x: 0, y: 10 },
+        region: {
+          bottom: 15,
+          left: 0,
+          right: 0,
+          top: 10,
+        },
+      });
+
+      expect(result).toBe(true);
+    });
+
+    it('on bottom of the region: true', () => {
+      const result = isOnTheRegion({
+        position: { x: 0, y: 15 },
+        region: {
+          bottom: 15,
+          left: 0,
+          right: 0,
+          top: 10,
+        },
+      });
+
+      expect(result).toBe(true);
+    });
+
+    it('on middle of the region: true', () => {
+      const result = isOnTheRegion({
+        position: { x: 0, y: 10 },
+        region: {
+          bottom: 15,
+          left: 0,
+          right: 0,
+          top: 5,
+        },
+      });
+
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('getRelativePosition', () => {
+    it('stay on zero', () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 0, y: 0 },
+      });
+
+      const expected: Axes = { x: 0, y: 0 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it('out of zero to {2,2}', () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+      });
+
+      const expected: Axes = { x: 2, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it('last {2,2} to {7,10}: {5,8}', () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 7, y: 10 },
+        lastPosition: { x: 2, y: 2 },
+      });
+
+      const expected: Axes = { x: 5, y: 8 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it('last {7,10} to {2,2}: {-5,-8}', () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+      });
+
+      const expected: Axes = { x: -5, y: -8 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it('relative to {10,10} last {7,10} to {2,2}: {5,2}', () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+      });
+
+      const expected: Axes = { x: 5, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit right 2: {2,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { right: 2 },
+      });
+
+      const expected: Axes = { x: 2, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit right 2, bottom 2: {2,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { right: 2, bottom: 2 },
+      });
+
+      const expected: Axes = { x: 2, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit right 2, bottom 1: {2,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { right: 2, bottom: 1 },
+      });
+
+      const expected: Axes = { x: 2, y: 1 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit right 8, bottom 8: {5,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { right: 8, bottom: 8 },
+      });
+
+      const expected: Axes = { x: 5, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit right 0, bottom 0: {0,0}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { right: 0, bottom: 0 },
+      });
+
+      const expected: Axes = { x: 0, y: 0 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit left 10, bottom 0: {10,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { left: 10, bottom: 10 },
+      });
+
+      const expected: Axes = { x: 10, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit left 10, top 1: {10,2}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { left: 10, top: 1 },
+      });
+
+      const expected: Axes = { x: 10, y: 2 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit left 10, top 8: {10,8}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { left: 10, top: 8 },
+      });
+
+      const expected: Axes = { x: 10, y: 8 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit bottom 10, top 8: {5,8}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { bottom: 10, top: 8 },
+      });
+
+      const expected: Axes = { x: 5, y: 8 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+
+    it(`
+      relative to {10,10} last {7,10} to {2,2}
+      with limit bottom 1, top 0: {5,1}
+    `, () => {
+      const relativePosition = getRelativePosition({
+        position: { x: 2, y: 2 },
+        lastPosition: { x: 7, y: 10 },
+        lastRelativePosition: { x: 10, y: 10 },
+        limit: { bottom: 1, top: 0 },
+      });
+
+      const expected: Axes = { x: 5, y: 1 };
+
+      expect(relativePosition).toStrictEqual(expected);
+    });
+  });
+
+  describe('isSafe', () => {
+    it('no debounce to be false', () => {
+      const safe = isSafe({
+        debounce: { x: 0, y: 0 },
+        lastPosition: { x: 0, y: 0 },
+        position: { x: 1, y: 1 },
+      });
+
+      expect(safe).toBe(false);
+    });
+
+    it('debounce 10 and moved 20 from 0 to be false', () => {
+      const safe = isSafe({
+        debounce: { x: 10, y: 10 },
+        lastPosition: { x: 0, y: 0 },
+        position: { x: 20, y: 20 },
+      });
+
+      expect(safe).toBe(false);
+    });
+
+    it('debounce 10 and moved 22 from 10 to be false', () => {
+      const safe = isSafe({
+        debounce: { x: 10, y: 10 },
+        lastPosition: { x: 10, y: 10 },
+        position: { x: 22, y: 22 },
+      });
+
+      expect(safe).toBe(false);
+    });
+
+    it('debounce 10 and moved 20 from 10 to be false', () => {
+      const safe = isSafe({
+        debounce: { x: 10, y: 10 },
+        lastPosition: { x: 10, y: 10 },
+        position: { x: 20, y: 20 },
+      });
+
+      expect(safe).toBe(false);
+    });
+
+    it('debounce 11 and moved 20 from 10 to be true', () => {
+      const safe = isSafe({
+        debounce: { x: 11, y: 11 },
+        lastPosition: { x: 10, y: 10 },
+        position: { x: 20, y: 20 },
+      });
+
+      expect(safe).toBe(true);
+    });
+  });
+
+  describe('isOnGapOfEl el 1000px and sreen 200px', () => {
+    const el = { ...document.createElement('div') };
+
+    el.scrollHeight = 1000;
+    el.clientHeight = 200;
+    el.scrollWidth = 1000;
+    el.clientWidth = 200;
+
+    it('gap 0 and positon 0 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+        },
+        position: { x: 0, y: 0 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('no gap and positon 0 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+        },
+        position: { x: 0, y: 0 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('no gap and positon 100 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+        },
+        position: { x: 100, y: 100 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('gap 0 and positon 100 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+        },
+        position: { x: 100, y: 100 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('gap 10 and positon 100 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 10,
+          left: 10,
+          right: 10,
+          top: 10,
+        },
+        position: { x: 100, y: 100 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('gap bottom 200 of 1000 and positon y 900 to be true', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 200,
+          left: 10,
+          right: 10,
+          top: 10,
+        },
+        position: { x: 100, y: 900 },
+      });
+
+      expect(onGap).toBe(true);
+    });
+
+    it('gap bottom 200 of 1000 and positon y 100 to be true', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 200,
+          left: 10,
+          right: 10,
+          top: 200,
+        },
+        position: { x: 100, y: 100 },
+      });
+
+      expect(onGap).toBe(true);
+    });
+
+    it('gap left 200 of 1000 and positon x 100 to be true', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 200,
+          right: 10,
+          top: 0,
+        },
+        position: { x: 100, y: 100 },
+      });
+
+      expect(onGap).toBe(true);
+    });
+
+    it('gap left 200 of 1000 and positon x 210 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 200,
+          right: 10,
+          top: 0,
+        },
+        position: { x: 210, y: 100 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('gap right 200 of 1000 and positon x 600 to be false', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 200,
+          top: 0,
+        },
+        position: { x: 600, y: 100 },
+      });
+
+      expect(onGap).toBe(false);
+    });
+
+    it('gap right 200 of 1000 and positon x 900 to be true', () => {
+      const onGap = isOnGapOfEl({
+        el,
+        gap: {
+          bottom: 0,
+          left: 0,
+          right: 200,
+          top: 0,
+        },
+        position: { x: 900, y: 100 },
+      });
+
+      expect(onGap).toBe(true);
     });
   });
 });
--- src/v2/utilities/axis/axis.ts
+++ src/v2/utilities/axis/axis.ts
@@ -3,8 +3,12 @@
 import { pipe, UnaryFunction, Observable } from 'rxjs';
 import { filter, map, pairwise, scan } from 'rxjs/operators';
 import intersection from 'lodash/intersection';
+import isNil from 'lodash/isNil';
+import isNumber from 'lodash/isNumber';
 import { getViewportHeight } from '../view/view.utilities';
-import { AXES, Axes, Direction } from './axis.types';
+import { AXES, Axes, Direction, POSITIONS } from './axis.types';
+import { Position } from '../../../types/types';
+import { getMaxHorizontalScroll, getMaxVerticalScroll } from '../element';
 
 export function getDirection({
   lastAxes,
@@ -259,3 +263,132 @@ export function putDirectionOperator<T>(
     }),
   );
 }
+
+export interface GetRelativeScrollPositionArgs {
+  position: Axes;
+  lastRelativePosition?: Axes;
+  lastPosition?: Axes;
+  limit?: Partial<Position>;
+}
+
+export type GetRelativePosition = (args: GetRelativeScrollPositionArgs) => Axes;
+
+export const getRelativePosition: GetRelativePosition = ({
+  position,
+  lastRelativePosition = { x: 0, y: 0 },
+  lastPosition = { x: 0, y: 0 },
+  limit,
+}) => {
+  let x = lastRelativePosition.x + position.x - lastPosition.x;
+  let y = lastRelativePosition.y + position.y - lastPosition.y;
+
+  if (limit) {
+    if (isNumber(limit.top)) y = Math.max(y, limit.top);
+    if (isNumber(limit.bottom)) y = Math.min(y, limit.bottom);
+    if (isNumber(limit.left)) x = Math.max(x, limit.left);
+    if (isNumber(limit.right)) x = Math.min(x, limit.right);
+  }
+
+  return {
+    x,
+    y,
+  };
+};
+
+export interface IsOnTheRegionArgs {
+  region: Position;
+  position: Axes;
+}
+
+export type IsOnTheRegion = (args: IsOnTheRegionArgs) => boolean;
+
+export const isOnTheRegion: IsOnTheRegion = ({ region, position }) => {
+  const onBelowTheTop = position.y >= region.top;
+  const onAboveTheBottom = position.y <= region.bottom;
+
+  return onBelowTheTop && onAboveTheBottom;
+};
+
+export interface IsOutOfLimitArgs {
+  limit?: Partial<Position>;
+  position: Axes;
+}
+
+export type IsOutOfLimit = (args: IsOutOfLimitArgs) => boolean;
+
+export const isOutOfLimit: IsOutOfLimit = ({ position, limit = POSITIONS }) => {
+  if (limit.top) {
+    const outOfTopLimit = position.y < limit.top;
+
+    if (outOfTopLimit) return true;
+  }
+
+  if (limit.bottom) {
+    const outOfBottomLimit = position.y > limit.bottom;
+
+    if (outOfBottomLimit) return true;
+  }
+
+  if (limit.left) {
+    const outOfLeftLimit = position.x < limit.left;
+
+    if (outOfLeftLimit) return true;
+  }
+
+  if (limit.right) {
+    const outOfRightLimit = position.x > limit.right;
+
+    if (outOfRightLimit) return true;
+  }
+
+  return false;
+};
+
+interface IsSafeArgs {
+  position: Axes;
+  lastPosition: Axes;
+  debounce: Partial<Axes>;
+}
+
+export type IsSafe = (args: IsSafeArgs) => boolean;
+
+export const isSafe: IsSafe = ({ position, lastPosition, debounce }) => {
+  const beOnVerticalSafe =
+    Math.abs(position.y - lastPosition.y) < (debounce.y ?? 0);
+  const beOnHorizontalSafe =
+    Math.abs(position.x - lastPosition.x) < (debounce.x ?? 0);
+
+  if (beOnVerticalSafe && beOnHorizontalSafe) return true;
+
+  return false;
+};
+
+interface IsOnGapArgs {
+  position: Axes;
+  gap: Partial<Position>;
+  el: HTMLElement;
+}
+
+export type IsOnGap = (args: IsOnGapArgs) => boolean;
+
+export const isOnGapOfEl: IsOnGap = ({ position, gap, el }) => {
+  const beOnTopGap = position.y < (gap.top ?? 0);
+
+  if (!isNil(gap.top) && beOnTopGap) return true;
+
+  const beOnBottomGap =
+    position.y > getMaxVerticalScroll(el) - (gap.bottom ?? 0);
+
+  if (!isNil(gap.bottom) && beOnBottomGap) return true;
+
+  const beOnLeftGap = position.x < (gap.left ?? 0);
+
+  if (!isNil(gap.left) && beOnLeftGap) return true;
+
+  const beOnRightGap =
+    position.x > getMaxHorizontalScroll(el) - (gap.right ?? 0);
+
+  if (!isNil(gap.right) && beOnRightGap) return true;
+
+  return false;
+};
