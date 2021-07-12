de3f931019f61da0fd1cf7052d11ceebe28d9ec1
jomarcardoso <antonionetocardoso@gmail.com>
2021-07-11 23:27:14 -0300

    feat: 🎸 remove old element files

--- src/utilities/element/sample/index.ts
+++
@@ -1,8 +0,0 @@
-// import './main.scss';
-// import { toggleDocumentScroll } from '../element.utilities';
-
-// const elButton = document.querySelector('button');
-
-// if (elButton) {
-//   elButton.addEventListener('click', () => toggleDocumentScroll({}));
-// }
