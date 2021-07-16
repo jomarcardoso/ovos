47337beed8cf79460af8db302a42741b7d3da8be
jomarcardoso <antonionetocardoso@gmail.com>
2021-07-16 00:27:39 -0300

    feat: 🎸 remove old carousel sample

--- src/interface/carousel/carousel.ts
+++ src/interface/carousel/carousel.ts
@@ -2,7 +2,7 @@ import './carousel.scss';
 import { Axis } from '../../utilities/axis';
 import scrollSpy, { createScrollSpyItem } from '../scroll-spy/scroll-spy';
 import { Method } from '../scroll-spy/scroll-spy.types';
-import { CreateSlide, Carousel, CarouselFitType } from './types/carousel.type';
+import { CreateSlide, Carousel, CarouselFitType } from './carousel.type';
 
 interface CustomCSSStyleDeclaration extends CSSStyleDeclaration {
   scrollSnapAlign: string;
